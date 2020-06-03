62715edbe72c536f3194903871436fab19a10848
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-03 22:08:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   @@all = []
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
   
   def self.all
@@ -15,5 +15,9 @@ class Dog
   
   def self.print_all
     @@all.each{|dog| puts dog.name}
+  end
+  
+  def save
+    @@all << self
   end
 end
